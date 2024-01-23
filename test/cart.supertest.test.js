85c24f05b1d11056cf96548204ebb3cee6459d78
Florencia <florencia.ohanian@gmail.com>
2024-01-23 16:36:36 -0300

    testing

--- test/cart.supertest.test.js
+++ test/cart.supertest.test.js
@@ -7,8 +7,10 @@ await mongoose.connect(process.env.MONGO_URL);
 
 const requester = supertest('http://localhost:8080')
 
+let newCartId = null;
+let idProducto = '651c088c7e9e111a88d6c96a';
 describe('Test CRUD supertest en ruta api/cart', function(){
-    it('iniciar sesion con post a traves de /login', async function() { // Increase timeout here 
+    it('Iniciar sesion con POST a traves de /login', async function() { 
         this.timeout(5000)
         const user = {
             "nombre": "Josefina",
@@ -16,9 +18,26 @@ describe('Test CRUD supertest en ruta api/cart', function(){
             "email": "jose.torres@gmail.com",
             "password": "jose159"
         };
-        const { statusCode, _body, ok} = await requester.post('/api/sessions/login').send(user);
-        token = _body.token;
-        // expect(statusCode).to.be.equal(200);
-        expect(token).to.be.ok;
+        const result = await requester.post('/api/sessions/login').send(user);
+        const cookieResult = result.headers['set-cookie'][0]
+        expect(cookieResult).to.be.ok;
+})
+// it('Crear un cart mediante post ', async function() { 
+//     this.timeout(5000);
+//     const result = await requester.post('/api/cart')
+//     console.log('Result', result.body.mensaje)
+//     newCartId = result.body.mensaje._id;
+//     expect(newCartId).to.be.ok;
+    
+// });
+
+it('Agregar producto a un cart mediante POST en /cart/:cid/productos/:pid', async function() {
+    this.timeout(5000);
+    const cantidad = {
+        cantidad: 1
+    };
+    const result = await requester.post(`/api/cart/${newCartId}/productos/${idProducto}`).send(cantidad)
+    console.log('idProducto',result.body)
+    expect(result.body).to.be.ok
 })
 })
