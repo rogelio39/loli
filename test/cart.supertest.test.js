de967c84106667d8f22a84094ef9b6cb8c8c2c5a
Florencia <florencia.ohanian@gmail.com>
2024-01-24 17:38:17 -0300

    testing

--- test/cart.supertest.test.js
+++ test/cart.supertest.test.js
@@ -22,21 +22,21 @@ describe('Test CRUD supertest en ruta api/cart', function(){
         const cookieResult = result.headers['set-cookie'][0]
         expect(cookieResult).to.be.ok;
 })
-// it('Crear un cart mediante post ', async function() { 
-//     this.timeout(5000);
-//     const result = await requester.post('/api/cart')
-//     console.log('Result', result.body.mensaje)
-//     newCartId = result.body.mensaje._id;
-//     expect(newCartId).to.be.ok;
+it('Crear un cart mediante POST ', async function() { 
+    this.timeout(5000);
+    const result = await requester.post('/api/cart')
+    console.log('Result', result.body.mensaje)
+    newCartId = result.body.mensaje._id;
+    expect(newCartId).to.be.ok;
     
-// });
+});
 
 it('Agregar producto a un cart mediante POST en /cart/:cid/productos/:pid', async function() {
     this.timeout(5000);
     const cantidad = {
         cantidad: 1
     };
-    const result = await requester.post(`/api/cart/${newCartId}/productos/${idProducto}`).send(cantidad)
+    const result = await requester.post(`/api/cart/${newCartId}/productos/${idProducto}`).set("jwtCookie",` ${cookie.result}`).send(cantidad)
     console.log('idProducto',result.body)
     expect(result.body).to.be.ok
 })
